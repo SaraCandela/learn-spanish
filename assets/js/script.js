b081c9292ff18d68430c82aebfcc7b6bdec3ad95
Sara Candela <saracandela15@gmail.com>
2022-07-01 14:35:49 +0000

    Correct some misspelling with the code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,176 +1,181 @@
-// create const
-let question = document.createElement('div')
+// Create const
+// Game questions and answers
 const gameData = [
     {
         question: `<img class="question" id="image" src="assets/images/cat.jpg">
-        <p class="question" id="animal"><b>Cat</b></p>`,
-        a: "Gato",
-        b: "Perro",
-        c: "Buho",
-        d: "Cerdo",
-        answer: "a"
+        <p class="question"><b>Cat</b></p>`,
+        answer:
+            `<button class="btn">Gato</button> 
+            <button class="btn">Perro</button> 
+            <button class="btn">Búho</button> 
+            <button class="btn">Cerdo</button>`,
+        corretAnswer: "a"
 
     },
     {
         question: `<img class="question" id="image" src="assets/images/cow.jpg">
-        <p class="question" id="animal"><b>Cow</b></p>`,
-        a: "Pato",
-        b: "Vaca",
-        c: "Caballo",
-        d: "Oveja",
-        answer: "b"
+        <p class="question"><b>Cow</b></p>`,
+        answer: `<button class="btn">Pato</button> 
+            <button class="btn">Vaca</button> 
+            <button class="btn">Caballo</button> 
+            <button class="btn">Oveja</button>`,
+        correctAnswer: "b"
 
     },
     {
         question: `<img class="question" id="image" src="assets/images/dog.jpg">
-        <p class="question" id="animal"><b>Dog</b></p>`,
-        a: "Gato",
-        b: "Perro",
-        c: "Mono",
-        d: "Rana",
-        answer: "b"
+        <p class="question"><b>Dog</b></p>`,
+        answer:`<button class="btn">Gato</button> 
+        <button class="btn">Perro</button> 
+        <button class="btn">Mono</button> 
+        <button class="btn">Rana</button>`,
+        correctAnswer: "b"
 
     },
     {
         question: `<img class="question" id="image" src="assets/images/donkey.jpg">
-        <p class="question" id="animal"><b>Donkey</b></p>`,
-        a: "Gato",
-        b: "Ardilla",
-        c: "Burro",
-        d: "Rana",
-        answer: "c"
+        <p class="question"><b>Donkey</b></p>`,
+        answer:`<button class="btn">Gato</button> 
+        <button class="btn">Ardilla</button> 
+        <button class="btn">Burro</button> 
+        <button class="btn">Rana</button>`,
+        correctAnswer: "c"
     },
     {
         question: `<img class="question" id="image" src="assets/images/duck.jpg">
-        <p class="question" id="animal"><b>Duck</b></p>`,
-        a: "Gato",
-        b: "Burro",
-        c: "Mono",
-        d: "Pato",
-        answer: "d"
+        <p class="question"><b>Duck</b></p>`,
+        answer:`<button class="btn">Gato</button> 
+        <button class="btn">Burro</button> 
+        <button class="btn">Mono</button> 
+        <button class="btn">Pato</button>`,
+        correctAnswer: "d"
     },
     {
         question: `<img class="question" id="image" src="assets/images/eagle.jpg">
-        <p class="question" id="animal"><b>Eagle</b></p>`,
-        a: "Aguila",
-        b: "Erizo",
-        c: "Mono",
-        d: "Rana",
-        answer: "a"
+        <p class="question"><b>Eagle</b></p>`,
+        answer:`<button class="btn">Aguila</button> 
+        <button class="btn">Erizo</button></button> 
+        <button class="btn">Mono</button> 
+        <button class="btn">Ardilla</button>`,
+        correctAnswer: "a"
     },
 
     {
         question: `<img class="question" id="image" src="assets/images/fox.jpg">
-        <p class="question" id="animal"><b>Fox</b></p>`,
-        a: "Aguila",
-        b: "Erizo",
-        c: "Zorro",
-        d: "Ardilla",
-        answer: "c"
+        <p class="question"><b>Fox</b></p>`,
+        answer:`<button class="btn">Buho</button> 
+        <button class="btn">Erizo</button> 
+        <button class="btn">Zorro</button> 
+        <button class="btn">Perro</button>`,
+        correctAnswer: "c"
     },
     {
         "question": `<img class="question" id="image" src="assets/images/frog.jpg">
-        <p class="question" id="animal"><b>Frog</b></p>`,
-        a: "Aguila",
-        b: "Erizo",
-        c: "Buho",
-        d: "Rana",
-        answer: "d"
+        <p class="question"><b>Frog</b></p>`,
+        answer:`<button class="btn">Buho</button> 
+        <button class="btn">Erizo</button> 
+        <button class="btn">Aguila</button> 
+        <button class="btn">Rana</button>`,
+        correctAnswer: "d"
     },
     {
         question: `<img class="question" id="image" src="assets/images/hedgehog.jpg">
-        <p class="question" id="animal"><b>Hedgehog</b></p>`,
-        a: "Aguila",
-        b: "Erizo",
-        c: "Mono",
-        d: "Caballo",
-        answer: "b"
+        <p class="question"><b>Hedgehog</b></p>`,
+        answer:`<button class="btn">Gato</button> 
+        <button class="btn">Erizo</button> 
+        <button class="btn">Mono</button> 
+        <button class="btn">Caballo</button>`,
+        correctAnswer: "b"
     },
     {
         question: `<img class="question" id="image" src="assets/images/horse.jpg">
-        <p class="question" id="animal"><b>Horse</b></p>`, 
-        a: "Gato",
-        b: "Caballo",
-        c: "Mono",
-        d: "Perro",
-        answer: "b"
+        <p class="question"><b>Horse</b></p>`, 
+        answer:`<button class="btn">Gato</button> 
+        <button class="btn">Caballo</button> 
+        <button class="btn">Rana</button> 
+        <button class="btn">Perro</button>`,
+        correctAnswer: "b"
     },
     {
         question: `<img class="question" id="image" src="assets/images/monkey.jpg">
-        <p class="question" id="animal"><b>Monkey</b></p>`, 
-        a: "Cerdo",
-        b: "Oveja",
-        c: "Mono",
-        d: "Vaca",
-        answer: "c"
+        <p class="question"><b>Monkey</b></p>`, 
+        answer:`<button class="btn">Cerdo</button> 
+        <button class="btn">Oveja</button> 
+        <button class="btn">Mono</button> 
+        <button class="btn">Vaca</button>`,
+        correctAnswer: "c"
     },
     {
         question: `<img class="question" id="image" src="assets/images/owl.jpg">
-        <p class="question" id="animal"><b>Owl</b></p>`,
-        a: "Oveja",
-        b: "Buho",
-        c: "Mono",
-        d: "Pato",
-        answer: "b"
+        <p class="question"><b>Owl</b></p>`,
+        answer:`<button class="btn">Caballo</button> 
+        <button class="btn">Buho</button> 
+        <button class="btn">Pato</button> 
+        <button class="btn">Rana</button>`,
+        correctAnswer: "b"
     },
     {
         question: `<img class="question" id="image" src="assets/images/pig.jpg">
-        <p class="question" id="animal"><b>Pig</b></p>`,
-        a: "Cerdo",
-        b: "Caballo",
-        c: "Gato",
-        d: "Vaca",
-        answer: "a"
+        <p class="question"><b>Pig</b></p>`,
+        answer:`<button class="btn">Cerdo</button> 
+        <button class="btn">Caballo</button> 
+        <button class="btn">Gato</button> 
+        <button class="btn">Vaca</button>`,
+        correctAnswer: "a"
     },
     {
         question: `<img class="question" id="image" src="assets/images/sheep.jpg">
-        <p class="question" id="animal"><b>Sheep</b></p>`,
-        a: "Burro",
-        b: "Oveja",
-        c: "Ardilla",
-        d: "Vaca",
-        answer: "b"
+        <p class="question"><b>Sheep</b></p>`,
+        answer:`<button class="btn">Burro</button> 
+        <button class="btn">Oveja</button> 
+        <button class="btn">Ardilla</button> 
+        <button class="btn">Vaca</button>`,
+        correctAnswer: "b"
     },
     {
         question: `<img class="question" id="image" src="assets/images/squirrel.jpg">
-        <p class="question" id="animal"><b>Squirrel</b></p>`,
-        a: "Perro",
-        b: "Oveja",
-        c: "Mono",
-        d: "Ardilla",
-        answer: "d"
+        <p class="question"><b>Squirrel</b></p>`,
+        answer:`<button class="btn">Perro</button> 
+        <button class="btn">Oveja</button> 
+        <button class="btn">Mono</button> 
+        <button class="btn">Ardilla</button>`,
+        correctAnswer: "d"
     },    
 ];
+
 const gameArea = document.getElementById('game-area');
 const questionContainer = document.getElementById('question-container');
 const answers = document.getElementById('answer-buttons');
 const score = document.getElementsByClassName('score-area');
 const next = document.getElementById ('next');
 
-let questionCounter = 0
+let answer = document.createElement ('section')
+let question = document.createElement('section')
+let availableQuestion = [];
 let scores = 0
 let currentQuestion;
-let availableQuestion = [];
 
-//call the functions. 
+//Call functions. 
 
 setAvailableQuestion();
 getNewQuestion();
 
-
-// push the questions into availableQuestion Array
+//Functions
+//Push the questions and answers into availableQuestion Array
 function setAvailableQuestion(){
     const totalQuestion = gameData.length;
      for(let i=0; i<totalQuestion; i++){
         availableQuestion.push(gameData[i])
      }
 }
-//set question and get random question
+//Set and get random question/answers
 function getNewQuestion() {
     const questionIndex = availableQuestion[Math.floor(Math.random() * availableQuestion.length)];
     currentQuestion = questionIndex;
     questionContainer.innerHTML = currentQuestion.question;
+    answers.innerHTML = currentQuestion.answer;
 }
+
 
 function selectAnswer(){
 
