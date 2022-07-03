58a52403b08e83e5f10938e98da6b3e17105753c
Sara Candela <saracandela15@gmail.com>
2022-07-03 12:32:31 +0000

    Create function for the Next button and the question Counter, as well as add a new js file to add question asn answers.

---
+++ assets/js/quiz.js
@@ -0,0 +1,96 @@
+
+const gameData = [
+    {
+        question: `<img class="question" id="image" src="assets/images/cat.jpg">
+        <p class="question"><b>Cat</b></p>`,
+        options: ['gato', 'perro', 'buho', 'cerdo'],
+        answer: 0
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/cow.jpg">
+        <p class="question"><b>Cow</b></p>`,
+        options: ['pato', 'vaca', 'caballo', 'oveja'],
+        answer: 1
+
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/dog.jpg">
+        <p class="question"><b>Dog</b></p>`,
+        options: ['gato', 'perro', 'mono', 'rana'],
+        answer: 1
+
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/donkey.jpg">
+        <p class="question"><b>Donkey</b></p>`,
+        options: ['gato', 'ardilla', 'burro', 'vaca'],
+        answer: 2
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/duck.jpg">
+        <p class="question"><b>Duck</b></p>`,
+        options: ['gato', 'burro', 'mono', 'pato'],
+        answer: 3,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/eagle.jpg">
+        <p class="question"><b>Eagle</b></p>`,
+        options: ['aguila', 'erizo', 'mono', 'ardilla'],
+        answer: 0,
+    },
+
+    {
+        question: `<img class="question" id="image" src="assets/images/fox.jpg">
+        <p class="question"><b>Fox</b></p>`,
+        options: ['buho', 'erizo', 'zorro', 'perro'],
+        answer: 2,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/frog.jpg">
+        <p class="question"><b>Frog</b></p>`,
+        options: ['buho', 'erizo', 'aguila', 'rana'],
+        answer: 3,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/hedgehog.jpg">
+        <p class="question"><b>Hedgehog</b></p>`,
+        options: ['gato', 'erizo', 'mono', 'caballo'],
+        answer: 1,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/horse.jpg">
+        <p class="question"><b>Horse</b></p>`, 
+        options: ['gato', 'caballo', 'rana', 'perro'],
+        answer: 1,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/monkey.jpg">
+        <p class="question"><b>Monkey</b></p>`, 
+        options: ['cerdo', 'oveja', 'mono', 'vaca'],
+        answer: 2,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/owl.jpg">
+        <p class="question"><b>Owl</b></p>`,
+        options: ['caballo', 'buho', 'pato', 'rana'],
+        answer: 1,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/pig.jpg">
+        <p class="question"><b>Pig</b></p>`,
+        options: ['cerdo', 'caballo', 'gato', 'vaca'],
+        answer: 0,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/sheep.jpg">
+        <p class="question"><b>Sheep</b></p>`,
+        options: ['burro', 'oveja', 'ardilla', 'vaca'],
+        answer: 1,
+    },
+    {
+        question: `<img class="question" id="image" src="assets/images/squirrel.jpg">
+        <p class="question"><b>Squirrel</b></p>`,
+        options: ['perro', 'oveja', 'mono', 'ardilla'],
+        answer: 3,
+    },    
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,196 +1,48 @@
-// Create const
-// Game questions and answers
-const gameData = [
-    {
-        question: `<img class="question" id="image" src="assets/images/cat.jpg">
-        <p class="question"><b>Cat</b></p>`,
-        answer:
-            `<button class="btn">Gato</button> 
-            <button class="btn">Perro</button> 
-            <button class="btn">Búho</button> 
-            <button class="btn">Cerdo</button>`,
-        corretAnswer: "a"
+const questionNumber = document.getElementById ('number')
+const questionContainer = document.getElementById('question-container')
+const answerButtons = document.getElementById ('answer-buttons')
+const scoreArea = document.getElementById ('score-area')
+const nextButton = document.getElementById ('next')
 
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/cow.jpg">
-        <p class="question"><b>Cow</b></p>`,
-        answer: `<button class="btn">Pato</button> 
-            <button class="btn">Vaca</button> 
-            <button class="btn">Caballo</button> 
-            <button class="btn">Oveja</button>`,
-        correctAnswer: "b"
-
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/dog.jpg">
-        <p class="question"><b>Dog</b></p>`,
-        answer:`<button class="btn">Gato</button> 
-        <button class="btn">Perro</button> 
-        <button class="btn">Mono</button> 
-        <button class="btn">Rana</button>`,
-        correctAnswer: "b"
-
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/donkey.jpg">
-        <p class="question"><b>Donkey</b></p>`,
-        answer:`<button class="btn">Gato</button> 
-        <button class="btn">Ardilla</button> 
-        <button class="btn">Burro</button> 
-        <button class="btn">Rana</button>`,
-        correctAnswer: "c"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/duck.jpg">
-        <p class="question"><b>Duck</b></p>`,
-        answer:`<button class="btn">Gato</button> 
-        <button class="btn">Burro</button> 
-        <button class="btn">Mono</button> 
-        <button class="btn">Pato</button>`,
-        correctAnswer: "d"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/eagle.jpg">
-        <p class="question"><b>Eagle</b></p>`,
-        answer:`<button class="btn">Aguila</button> 
-        <button class="btn">Erizo</button></button> 
-        <button class="btn">Mono</button> 
-        <button class="btn">Ardilla</button>`,
-        correctAnswer: "a"
-    },
-
-    {
-        question: `<img class="question" id="image" src="assets/images/fox.jpg">
-        <p class="question"><b>Fox</b></p>`,
-        answer:`<button class="btn">Buho</button> 
-        <button class="btn">Erizo</button> 
-        <button class="btn">Zorro</button> 
-        <button class="btn">Perro</button>`,
-        correctAnswer: "c"
-    },
-    {
-        "question": `<img class="question" id="image" src="assets/images/frog.jpg">
-        <p class="question"><b>Frog</b></p>`,
-        answer:`<button class="btn">Buho</button> 
-        <button class="btn">Erizo</button> 
-        <button class="btn">Aguila</button> 
-        <button class="btn">Rana</button>`,
-        correctAnswer: "d"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/hedgehog.jpg">
-        <p class="question"><b>Hedgehog</b></p>`,
-        answer:`<button class="btn">Gato</button> 
-        <button class="btn">Erizo</button> 
-        <button class="btn">Mono</button> 
-        <button class="btn">Caballo</button>`,
-        correctAnswer: "b"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/horse.jpg">
-        <p class="question"><b>Horse</b></p>`, 
-        answer:`<button class="btn">Gato</button> 
-        <button class="btn">Caballo</button> 
-        <button class="btn">Rana</button> 
-        <button class="btn">Perro</button>`,
-        correctAnswer: "b"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/monkey.jpg">
-        <p class="question"><b>Monkey</b></p>`, 
-        answer:`<button class="btn">Cerdo</button> 
-        <button class="btn">Oveja</button> 
-        <button class="btn">Mono</button> 
-        <button class="btn">Vaca</button>`,
-        correctAnswer: "c"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/owl.jpg">
-        <p class="question"><b>Owl</b></p>`,
-        answer:`<button class="btn">Caballo</button> 
-        <button class="btn">Buho</button> 
-        <button class="btn">Pato</button> 
-        <button class="btn">Rana</button>`,
-        correctAnswer: "b"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/pig.jpg">
-        <p class="question"><b>Pig</b></p>`,
-        answer:`<button class="btn">Cerdo</button> 
-        <button class="btn">Caballo</button> 
-        <button class="btn">Gato</button> 
-        <button class="btn">Vaca</button>`,
-        correctAnswer: "a"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/sheep.jpg">
-        <p class="question"><b>Sheep</b></p>`,
-        answer:`<button class="btn">Burro</button> 
-        <button class="btn">Oveja</button> 
-        <button class="btn">Ardilla</button> 
-        <button class="btn">Vaca</button>`,
-        correctAnswer: "b"
-    },
-    {
-        question: `<img class="question" id="image" src="assets/images/squirrel.jpg">
-        <p class="question"><b>Squirrel</b></p>`,
-        answer:`<button class="btn">Perro</button> 
-        <button class="btn">Oveja</button> 
-        <button class="btn">Mono</button> 
-        <button class="btn">Ardilla</button>`,
-        correctAnswer: "d"
-    },    
-];
-
-const gameArea = document.getElementById('game-area');
-const questionContainer = document.getElementById('question-container');
-const answers = document.getElementById('answer-buttons');
-const score = document.getElementsByClassName('score-area');
-const next = document.getElementById ('next');
-
-let answer = document.createElement ('section')
-let question = document.createElement('section')
-let availableQuestion = [];
-let scores = 0
+let questionCounter = 0
+let scoreCounter = 0;
 let currentQuestion;
+let availableQuestions= [];
 
-//Call functions. 
 
-setAvailableQuestion();
-getNewQuestion();
-
-//Functions
-//Push the questions and answers into availableQuestion Array
-function setAvailableQuestion(){
-    const totalQuestion = gameData.length;
-     for(let i=0; i<totalQuestion; i++){
-        availableQuestion.push(gameData[i])
-     }
+//push the questions into availableQuestions Array.
+function setavailableQuestions(){
+    const totalQuestions = gameData.length;
+    for (let i=0; i<totalQuestions; i++){
+        availableQuestions.push(gameData[i])
+    }
 }
-//Set and get random question/answers
-function getNewQuestion() {
-    const questionIndex = availableQuestion[Math.floor(Math.random() * availableQuestion.length)];
+
+//set question and options
+function newQuestion(){
+    //set question number
+    questionNumber.innerHTML = "Question " + (questionCounter + 1) + " of " + gameData.length;
+    // set question text
+    //get random question
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)]
     currentQuestion = questionIndex;
     questionContainer.innerHTML = currentQuestion.question;
-    answers.innerHTML = currentQuestion.answer;
+    console.log(questionIndex)
+    
+    questionCounter ++ 
+} 
+
+function next(){
+    if(questionCounter === gameData.length){
+        console.log("quiz over");
+    } else{
+        newQuestion();
+    }
 }
-
-
-function selectAnswer(){
-
-}
-
-
-function calculateCorrectAnswer(){
-
-}
-
-function incrementScore(){
-
-}
-
-function setNextQuestion(){
-
+window.onload = function() {
+    //first we will set all questions in availableQuestions Array
+    setavailableQuestions();
+    //second we will call newQuestion(); function
+    newQuestion();
 }
 
